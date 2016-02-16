571965d64e1c3879b9d84cce852bd8d63eff98c1
Godefroid Chapelle <gotcha@bubblenet.be>
2016-02-16 19:09:38 +0100

    Problem: .ssh needs appropriate permissions
    Solution: create it accordingly

--- mdh_init_and_commit.sh
+++ mdh_init_and_commit.sh
@@ -3,9 +3,8 @@
 set -ex
 
 DEST=/opt/sandbox/mdhtest
-ls -al  ~
-sudo ls -al  ~/.ssh
-cat ~/.ssh/known_hosts
+sudo cp -R /opt/.ssh /home/zmq/.ssh
+sudo chown -R zmq:zmq /home/zmq/.ssh
 python3 /opt/mdh.py init \
    --git-email=mdh@bubblenet.be  --git-name="Maitre d'hotel" \
    "$2" "${DEST}"
