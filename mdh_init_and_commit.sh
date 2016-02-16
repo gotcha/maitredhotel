97810c5e75ed757d12f79b83f61c8740c06eb390
Godefroid Chapelle <gotcha@bubblenet.be>
2016-02-16 18:25:35 +0100

    Problem: bad permissions
    Solution: use sudo ?

--- mdh_init_and_commit.sh
+++ mdh_init_and_commit.sh
@@ -4,7 +4,7 @@ set -ex
 
 DEST=/opt/sandbox/mdhtest
 ls -al  ~
-ls -al  ~/.ssh
+sudo ls -al  ~/.ssh
 cat ~/.ssh/known_hosts
 python3 /opt/mdh.py init \
    --git-email=mdh@bubblenet.be  --git-name="Maitre d'hotel" \
